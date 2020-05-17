0bd84ed35497c5201f270992c2b2434a3482bfd3
ljil <randomlycoding@gmail.com>
2020-05-17 13:07:35 +0400

    добавлена галерея фильма/человека

--- moviedb/movies/admin.py
+++ moviedb/movies/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from .models import (
-    Person,
-    Movie,
-    Genre
-)
+from . import models
 
-admin.site.register(Person)
-admin.site.register(Movie)
-admin.site.register(Genre)
+admin.site.register(models.Person)
+admin.site.register(models.Movie)
+admin.site.register(models.Genre)
+admin.site.register(models.Gallery)
--- moviedb/movies/models.py
+++ moviedb/movies/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 from django.urls import reverse
 
 
@@ -57,3 +55,12 @@ class Movie(models.Model):
     class Meta:
         verbose_name = "Фильм"
         verbose_name_plural = "Фильмы"
+
+
+class Gallery(models.Model):
+    image = models.ImageField("Фото", upload_to='frames')
+    to = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='gallery', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Фото"
+        verbose_name_plural = "Фото"
