0c15eb6417f29d1a6ef64caa2fc0985e676790d2
ljil <randomlycoding@gmail.com>
2020-06-02 17:59:36 +0400

    форма с жанрами

--- moviedb/movies/views.py
+++ moviedb/movies/views.py
@@ -2,10 +2,14 @@ from django.views import generic
 from . import models
 
 
-class MovieListView(generic.ListView):
+class GenresMixin:
+    def get_genres(self):
+        return models.Genre.objects.all()
+
+
+class MovieListView(generic.ListView, GenresMixin):
     def get_queryset(self):
         if self.request.GET.getlist('genre'):
-            print(self.request.GET.getlist('genre'))
             return models.Movie.objects.filter(
                 published=True,
                 genres__slug__in=self.request.GET.getlist('genre')
